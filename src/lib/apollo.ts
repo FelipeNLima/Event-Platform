5f033fe6ba1fc3e9e7789b55db2190d8555b619d
Felipe Lima <felipinho.lima27@gmail.com>
2022-06-29 21:35:34 -0300

    criando usuario

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o7s0eq10fo01z7961c7zuw/master',
+  uri: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "Authorization": `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 });
